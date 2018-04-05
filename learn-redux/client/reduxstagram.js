e8702fd9b2545a6b9a2bbd09d18406ad40ab3b21
David Thorpe <davidthorpe878@gmail.com>
2018-04-05 20:35:05 +0100

    Continued learning along to redux tutorial up to video 8

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import Main from './components/Main';
-
 import { render } from 'react-dom';
+
+// Import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 // Import CSS
 import css from './styles/style.styl';
 
-render(<Main />, document.getElementById('root'));
+
+const router = (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Main}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
+);
+
+render(router, document.getElementById('root'));
 
