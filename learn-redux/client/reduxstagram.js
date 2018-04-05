867d22b19744e13629e06d33d2a1432ffe261a8e
David Thorpe <davidthorpe878@gmail.com>
2018-04-05 19:36:43 +0100

    Made initial components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import Main from './components/Main';
+
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+render(<Main />, document.getElementById('root'));
+
